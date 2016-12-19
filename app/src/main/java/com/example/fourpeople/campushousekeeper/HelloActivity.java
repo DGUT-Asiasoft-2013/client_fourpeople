649b51ae031824e7b5bd8183251cf34be32017b4
fourpeople <huanglinrong1994@qq.com>
2016-12-19 11:29:12 +0800

    基本项目第一次上传

---
+++ app/src/main/java/com/example/fourpeople/campushousekeeper/HelloActivity.java
@@ -0,0 +1,13 @@
+package com.example.fourpeople.campushousekeeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
